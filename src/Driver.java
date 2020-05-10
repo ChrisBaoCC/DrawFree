8cadfe6d9737daa209ccc78882c29c0f77f9e65a
Chris Bao <chrisbaome@gmail.com>
2020-05-10 14:39:35 -0400

    Initial commit

---
+++ src/Driver.java
@@ -0,0 +1,12 @@
+/**
+ * <code>Driver</code> class. Runs the program.
+ */
+public class Driver {
+	/**
+	 * Main method. Entry point for the app to run.
+	 * @param args Arguments from the command line. Should be empty but doesn't matter.
+	 */
+	public static void main(String[] args) {
+		Frame f = new Frame();
+	}
+}
---
+++ src/Frame.java
@@ -0,0 +1,6 @@
+/**
+ * <code>Frame</code> class. Contains all the GUI for the app.
+ */
+public class Frame {
+	
+}
